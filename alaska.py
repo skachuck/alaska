ed5e01eaf9d16feb24df7bf2ba7d751c0492fb40
skachuck <skachuck@gmail.com>
2017-02-14 17:29:16 -0500

    new map params, loading data.wq

--- alaska.py
+++ alaska.py
@@ -1,17 +1,27 @@
+"""
+alaska.py
+
+Author: Samuel B. Kachuck
+Date  : 06 02 2017
+
+Master script for computing gia in alaska.
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 import giapy as gp
 
+# Setup map
 alaska = {'area_thresh': 1000.0,
-          'height': 4700000,
+          'height': 2300000,
           'lat_0': 57,
-          'lat_1': 50.0,
-          'lat_2': 89.9,
+#          'lat_1': 55.0,
+#          'lat_2': 85,
           'lon_0': -144.0,
           'projection': 'aeqd',
           'resolution': 'l',
           'rsphere': (6378137.0, 6356752.3142),
-          'width': 4900000}
+          'width': 2400000}
 
 grid = gp.maps.GridObject(mapparam=alaska)
 
@@ -20,11 +30,13 @@ ys = np.linspace(grid.basemap.ymin, grid.basemap.ymax)
 Xs, Ys = np.meshgrid(xs, ys)
 Lons, Lats = grid.basemap(Xs, Ys, inverse=True)
 
-dx = np.mean(giapy.maps.haversine(Lats[:-1,:-1], Lats[1:,1:],
-                                  Lons[:-1,:-1], Lons[1:,1:]))/np.sqrt(2)
+dx = np.mean(gp.maps.haversine(Lats[:-1,:-1], Lats[1:,1:],
+                               Lons[:-1,:-1], Lons[1:,1:]))/np.sqrt(2)
 
-fs = np.fft.fftfreq(N, d=dx)
+#fs = np.fft.fftfreq(N, d=dx)
 
 
 # Load dhdt data
+data = np.load('../alaska/data/glacier_bay_latlon.dat')
+tmp, lond, latd, rd, dhdt = data.T
 
